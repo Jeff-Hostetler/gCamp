1a10b52762d3651df6f8df7bea20b8d2b24ba8f2
Jeff Hostetler <hostetjm@gmail.com>
2014-10-15 14:40:51 -0600

    create FAQ page. git status HTML based ATM

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 root "pages#index"
 get "about" => "pages#about", name: :about
 get "terms" => "pages#terms", name: :terms
+get "faq" => "pages#faq", name: :faq
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
