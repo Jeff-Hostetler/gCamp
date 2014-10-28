952fae4989e0279133e2842c4cff72aa21816742
Jeff Hostetler <hostetjm@gmail.com>
2014-10-28 10:39:47 -0600

    added edit to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,14 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
     @users = User.all
+  end
+
+  def show
+  end
+
+  def edit
   end
 
   # GET /users/new
@@ -17,6 +24,24 @@ class UsersController < ApplicationController
         end
       end
   end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to users_url, notice: 'user was successfully updated.'}
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user
+      @user = User.find(params[:id])
+    end
 
   def user_params
       params.require(:user).permit(:first_name, :last_name, :email)
