9ea541f6138a8bb5fd7b5d081c24723216504719
Jeff Hostetler <hostetjm@gmail.com>
2014-10-29 15:09:10 -0600

    migration to set completed to false by default to solve sorting error

---
+++ db/migrate/20141029210508_add_default_false_to_complete_in_tasks.rb
@@ -0,0 +1,8 @@
+class AddDefaultFalseToCompleteInTasks < ActiveRecord::Migration
+  def up
+    change_column :tasks, :complete, :boolean, :default => false
+  end
+  def down
+    change_column :tasks, :complete, :boolean, :default => nil
+  end
+end
