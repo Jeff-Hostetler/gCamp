fe4faf468489bc16d0893ab75cb2c907bd087e94
Jeff Hostetler <hostetjm@gmail.com>
2014-10-30 14:30:13 -0600

    finished projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,4 +1,5 @@
 class ProjectsController <  ApplicationController
+  before_action :set_project, only: [:show, :edit, :update]
 
   def index
     @projects = Project.all
@@ -9,15 +10,35 @@ class ProjectsController <  ApplicationController
   end
 
   def create
-    @project = Project.new(project_param)
+    @project = Project.new(project_params)
     @project.save
-    redirect_to projects_path
+    redirect_to @project, notice: 'Project successfully created'
+  end
+
+  def show
+  end
+
+  def destroy
+    Project.find(params[:id]).destroy
+    redirect_to projects_path, notice: 'Project was successfully destroyed.'
+  end
+
+  def edit
+  end
+
+  def update
+    @project.update(project_params)
+    redirect_to @project, notice: 'Project was successfully updated.'
   end
 
   private
 
   def project_params
     params.require(:project).permit(:name)
+  end
+
+  def set_project
+    @project = Project.find(params[:id])
   end
 
 end
