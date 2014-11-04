29be3600c0f4620299c375997e344c0d125ff7e2
Jeff Hostetler <hostetjm@gmail.com>
2014-11-04 14:22:14 -0700

    added rspec and capybara

---
+++ spec/features/projects_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+  feature "projects" do
+
+    scenario "create project" do
+      visit root_path
+      click_on "Projects"
+
+      click_on "Create Project"
+
+
+
+
+
+
+
+  end
+
+end
