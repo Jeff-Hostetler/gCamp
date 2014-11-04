650ade6f63c5d63f00c8f84cf29808960f5f9bbc
Jeff Hostetler <hostetjm@gmail.com>
2014-11-04 12:48:52 -0700

    fixed create user from user page and added name to signup. user name up top right instead of email when logged in

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,7 +5,8 @@ class RegistrationsController < ApplicationController
   end
 
   def create
-    @user = User.new(params.require(:user).permit(:email, :password, :password_confirmation))
+    @user = User.new(params.require(:user).permit(:email, :password, :password_confirmation,
+    :first_name, :last_name))
     if @user.save
       session[:user_id] = @user.id
       redirect_to root_path
