a3d4abd4e2b3a0df431287989fb355a94afa328f
Jeff Hostetler <hostetjm@gmail.com>
2014-11-04 15:53:19 -0700

    new project test added

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -1,19 +1,22 @@
 require 'rails_helper'
 
-  feature "projects" do
+feature "projects" do
 
-    scenario "create project" do
-      visit root_path
-      click_on "Projects"
+  scenario "create project" do
+    visit root_path
+    expect(page).to have_content("Projects")
 
-      click_on "Create Project"
+    click_on "Projects"
+    expect(page).to have_no_content("TEST")
 
+    click_on "Create Project"
 
+    fill_in "Name", with: "This is a name"
+    click_on "Add Project"
 
-
-
-
-
+    expect(page).to have_content('This is a name')
+    expect(page).to have_content('Project successfully created')
   end
 
+  scenario "update project" d
 end
