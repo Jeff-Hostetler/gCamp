816a1fa7dc60cf076a25e333e158d1ea7a235b3b
Jeff Hostetler <hostetjm@gmail.com>
2014-11-20 14:21:59 -0700

    tests for membership

---
+++ spec/features/membership_spec.rb
@@ -0,0 +1,71 @@
+require 'rails_helper'
+
+feature 'membership' do
+
+  scenario 'can add valid membership' do
+    Project.create!(
+      name: "TEST"
+      )
+    User.create!(
+      first_name: "First",
+      last_name: "Last",
+      email: "test@test.com",
+      password: "password",
+      password_confirmation: "password"
+      )
+
+    visit root_path
+    click_on "Projects"
+    click_on "TEST"
+    click_on "0 Members"
+    select "First Last", from: "membership_user_id"
+    click_on "Add New Member"
+    expect(page).to have_content("First Last was added successfully")
+  end
+
+  scenario 'user attempts to add membership with no user assigned' do
+    Project.create!(
+    name: "TEST"
+    )
+    User.create!(
+    first_name: "First",
+    last_name: "Last",
+    email: "test@test.com",
+    password: "password",
+    password_confirmation: "password"
+    )
+
+    visit root_path
+    click_on "Projects"
+    click_on "TEST"
+    click_on "0 Members"
+    click_on "Add New Member"
+    expect(page).to have_content("User can't be blank")
+  end
+
+  scenario 'user attempts to add membership with a user that already has a membership' do
+    Project.create!(
+    name: "TEST"
+    )
+    User.create!(
+    first_name: "First",
+    last_name: "Last",
+    email: "test@test.com",
+    password: "password",
+    password_confirmation: "password"
+    )
+
+    visit root_path
+    click_on "Projects"
+    click_on "TEST"
+    click_on "0 Members"
+    select "First Last", from: "membership_user_id"
+    click_on "Add New Member"
+    expect(page).to have_content("First Last was added successfully")
+    select "First Last", from: "membership_user_id"
+    click_on "Add New Member"
+    expect(page).to have_content("User has already been taken")
+  end
+
+
+end
