20cbcecff3b4995ecdf0555f62587da1309d8861
Jeff Hostetler <hostetjm@gmail.com>
2014-11-20 21:39:52 -0700

    added membership update test

--- spec/features/membership_spec.rb
+++ spec/features/membership_spec.rb
@@ -67,5 +67,31 @@ feature 'membership' do
     expect(page).to have_content("User has already been taken")
   end
 
+  scenario 'membership can be updated' do
+    Project.create!(
+    name: "TEST"
+    )
+    User.create!(
+    first_name: "First",
+    last_name: "Last",
+    email: "test@test.com",
+    password: "password",
+    password_confirmation: "password"
+    )
+
+    visit root_path
+    click_on "Projects"
+    click_on "TEST"
+    click_on "0 Members"
+    select "First Last", from: "membership_user_id"
+    click_on "Add New Member"
+    expect(page).to have_content("First Last was added successfully")
+    within(".col-sm-3") do
+      select "admin", from: "membership_role"
+    end
+    click_on "Update"
+    expect(page).to have_content("First Last was updated successfully")
+  end
+
 
 end
