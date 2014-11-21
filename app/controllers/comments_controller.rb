08579172ce693610dfbca5b1b27859a0ac6dea80
Jeff Hostetler <hostetjm@gmail.com>
2014-11-21 15:36:27 -0700

    corrected bug for current_user=nil on create new task

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,9 @@ class CommentsController < ApplicationController
 
   def create
     @comment = @task.comments.new(params.require(:comment).permit(:user_id, :task_id, :description))
-    @comment.user_id = current_user.id
+    if current_user != nil
+      @comment.user_id = current_user.id
+    end
     if @comment.save
       flash[:notice] = "Comment was successfully created."
       redirect_to project_task_path(@project, @task )
