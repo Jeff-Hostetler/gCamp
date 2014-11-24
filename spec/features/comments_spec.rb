6f4ffcd5bd0e640bdf9108aac463398ecd8715cc
Jeff Hostetler <hostetjm@gmail.com>
2014-11-24 11:20:41 -0700

    fixed style bugs on signin/signup

--- spec/features/comments_spec.rb
+++ spec/features/comments_spec.rb
@@ -2,8 +2,69 @@ require 'rails_helper'
 
 feature 'comments' do
 
-  scenario "user can see add comment to a taks" do
-    
+  scenario "signed in user can see add comment to a task" do
+    Project.create!(
+      name: "TEST",
+      id: 1
+    )
+    Task.create!(
+      description: "TEST TASK",
+      project_id: 1
+    )
+    user = User.create!(
+      first_name: "First",
+      last_name: "Last",
+      email: "test@test.com",
+      password: "password",
+      password_confirmation: "password"
+    )
+    visit root_path
+    click_on "Sign In"
+    fill_in "Email", with: "test@test.com"
+    fill_in "Password", with: "password"
+    within (".well") do
+      click_on "Sign In"
+    end
+
+    visit root_path
+    click_on "Projects"
+    click_on "1"
+    click_on "TEST TASK"
+    fill_in "comment_description", with: "test description"
+    click_on "Add Comment"
+    expect(page).to have_content("Comment was successfully created")
+  end
+
+  scenario "signed in user attempts to create blank task" do
+    Project.create!(
+    name: "TEST",
+    id: 1
+    )
+    Task.create!(
+    description: "TEST TASK",
+    project_id: 1
+    )
+    user = User.create!(
+    first_name: "First",
+    last_name: "Last",
+    email: "test@test.com",
+    password: "password",
+    password_confirmation: "password"
+    )
+    visit root_path
+    click_on "Sign In"
+    fill_in "Email", with: "test@test.com"
+    fill_in "Password", with: "password"
+    within (".well") do
+      click_on "Sign In"
+    end
+
+    visit root_path
+    click_on "Projects"
+    click_on "1"
+    click_on "TEST TASK"
+    click_on "Add Comment"
+    expect(page).to have_no_content("First Last")
   end
 
 end
