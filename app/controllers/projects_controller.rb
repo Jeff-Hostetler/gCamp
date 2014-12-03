0d7e82889c1cede5d98481bfe0b73823171f1911
Jeff Hostetler <hostetjm@gmail.com>
2014-12-03 10:21:01 -0700

    fixed alignment on public pages

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,7 +1,7 @@
 class ProjectsController <  ApplicationController
   before_action :set_project, only: [:show, :edit, :update]
   before_action :current_user_has_project_permission, except: [:index, :new, :create, :destroy]
-  before_action :current_user_is_owner_to_edit, only: [:edit, :update, :destroy]
+  before_action :current_user_is_owner_to_edit, only: [:edit, :update]
 
   def index
     if current_user.admin == true
