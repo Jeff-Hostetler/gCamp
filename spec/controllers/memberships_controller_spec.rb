ced44c7e7d97c434eae322d4cc1362b4db141d3a
Jeff Hostetler <hostetjm@gmail.com>
2014-12-03 14:56:24 -0700

    added controller specs for memberships

--- spec/controllers/memberships_controller_spec.rb
+++ spec/controllers/memberships_controller_spec.rb
@@ -10,5 +10,37 @@ describe MembershipsController do
 
       expect(response).to redirect_to(login_path)
     end
+
+    it "allows members of projects to view memberships" do
+      project = create_project
+      user = create_user
+      membership = create_member(user, project)
+      session[:user_id] = user.id
+
+      get :index, {project_id: project.id}
+
+      expect(response).to be_success
+    end
+
+    it "allows admins to view memberships" do
+      project = create_project
+      admin = create_admin
+      session[:user_id] = admin.id
+
+      get :index, {project_id: project.id}
+
+      expect(response).to be_success
+    end
+
+    it "does not allow non-members of projects to view memberships" do
+      project = create_project
+      user = create_user
+
+      session[:user_id] = user.id
+
+      get :index, {project_id: project.id}
+
+      expect(response.status).to eq(404)
+    end
   end
 end
