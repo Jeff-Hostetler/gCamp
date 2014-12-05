07a41aef3e1a5d2467d2fbba518ec270f09f8f4a
Jeff Hostetler <hostetjm@gmail.com>
2014-12-05 14:45:45 -0700

    added edit task test coverage

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -94,17 +94,39 @@ describe TasksController do
     end
   end
 
-  # describe "#edit" do
-  #   it "allows user to edit thier own profile" do
-  #     user = create_user
-  #     project = create_project
-  #     task = create_task(project)
-  #     membership = create_member(user, project)
-  #     session[:user_id] = user.id
-  #
-  #     patch :update, project_id: project.id, id: task.id, params: valid
-  #
-  #     expect(response).to be_success
-  #   end
-  # end
+  describe "#edit" do
+    it "allows user to edit tasks within a project of which they are a member" do
+      user = create_user
+      project = create_project
+      task = create_task(project)
+      membership = create_member(user, project)
+      session[:user_id] = user.id
+
+      get :edit, project_id: project.id, id: task.id
+
+      expect(response).to be_success
+    end
+
+    it "does not allow a non-member to edit a task" do
+      user = create_user
+      project = create_project
+      task = create_task(project)
+      session[:user_id] = user.id
+
+      get :edit, project_id: project.id, id: task.id
+
+      expect(response.status).to eq(404)
+    end
+
+    it "allows admin to edit any task" do
+      admin = create_admin
+      project = create_project
+      task = create_task(project)
+      session[:user_id] = admin.id
+
+      get :edit, project_id: project.id, id: task.id
+
+      expect(response).to be_success
+    end
+  end
 end
